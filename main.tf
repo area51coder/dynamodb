a429fd01cc1a925e9be3894067698996b343c737
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-23 12:11:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ name = "LockID"
 type = "S"
 }
 }
+terraform {
+backend "s3" {
+# Replace this with your bucket name!
+bucket = "terraform-up-and-running-state-area51"
+key = "global/s3/terraform.tfstate"
+region = "us-east-2"
+# Replace this with your DynamoDB table name!
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt = true
+}
+}
